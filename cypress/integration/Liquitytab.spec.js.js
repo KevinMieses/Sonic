12aa531ff51bc5c2a7d73d2e785646d086e1d764
KevinMieses <93011588+KevinMieses@users.noreply.github.com>
2022-02-13 16:17:53 -0600

    Add files via upload

---
+++ cypress/integration/Liquitytab.spec.js.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+describe ('Search elements',()=>{
+
+before('',()=>{
+    cy.visit('https://2aoj2-aaaaa-aaaad-qa4qq-cai.ic.fleek.co/')
+
+})
+
+it('Liquity tab',()=>{
+       cy.get('#tabs-1--tab-1').click()
+
+        cy.get('.css-1oygcxo')
+          .should('be.visible')
+          .should('have.text','Liquidity Provider Rewards')
+
+        cy.get('.css-9svypg > .chakra-text')
+           .should('be.visible')
+           .should('have.text','Liquidity providers earn a 0.3% fee on all trades proportional to their share of the pool. Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity. If you want to learn review our documentation.')
+
+        cy.get('.css-22dskd')
+            .should('be.visible')
+            .should('have.text','Your Liquidity Positions')  
+
+            cy.get('.css-l0aeb3 > .chakra-button')
+              .should('be.visible')
+              .should('have.text','Create Position')
+
+              cy.get('.css-1ppz6m1 > svg') 
+                .should('be.visible')
+
+            cy.get('.css-1imtb7y > .chakra-stack > .chakra-text')
+              .should('be.visible')
+              .should('have.text','Your liquidity positions will appear here.')
+
+            cy.get('.css-1imtb7y > .chakra-stack > .chakra-button')
+                .should('be.visible')
+                .should('have.text','Install Plug')
+
+            cy.get('.css-9xuvt2')
+              .should('be.visible')
+              .should('have.text','Sonic v1.1.0') 
+            
+    })
+})
